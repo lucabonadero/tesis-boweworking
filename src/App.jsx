1e4e2f500703deb20236fda95e555cc38e9a5bca
1bonacci <valentinbonacci@gmail.com>
2025-05-05 20:59:23 -0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import RegistroCliente from "./pages/registrocliente";
 import Index from "./pages/index";
 import Espacios from "./pages/espacios";
 import ControlReservas from "./pages/consultareservas";
+import Login from "./pages/login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/registro" element={<RegistroCliente />} />
         <Route path="/espacios" element={<Espacios />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/control" element={<ControlReservas />} />
       </Routes>
     </BrowserRouter>
