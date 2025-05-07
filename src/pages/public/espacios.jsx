b6331dba46c74fb7d48f0c5d22b7038ab244fe5a
1bonacci <valentinbonacci@gmail.com>
2025-05-07 18:03:38 -0300

    jijo

--- src/pages/public/espacios.jsx
+++ src/pages/public/espacios.jsx
@@ -4,50 +4,73 @@ import Footer from "../../components/footer.jsx";
 import styles from "../../styles/public/espacios.module.css";
 import "../../styles/global.css";
 
-import imgprimerpiso from '../../assets/primerpiso.png';
-import imgsalareuniones from '../../assets/salareuniones.png';
-import imgterraza from '../../assets/terrazarda.png';
-import imgplanta from '../../assets/plantabaja.png';
+import img1 from '../../assets/espacios_sillas.png';
+import img2 from '../../assets/espacios_sillones.png';
+import img3 from '../../assets/oficina_individual.png';
+import img4 from '../../assets/salareuniones.png';
+import img5 from '../../assets/terrazarda.png';
+import img6 from '../../assets/espacios_plantabaja.png';
+
 
 const espacios = [
     {
-        titulo: 'Oficina Grande',
-        imagen: imgplanta,
+        titulo: 'Bancos',
+        imagen: img1,
         caracteristicas: [
-          'Capacidad hasta 8 personas',
-          'Salón privado',
+          'Individual',
+          'Espacio',
           'Proyector y pantalla',
           'Escritorios ergonómicos',
         ],
     },
     {
-      titulo: 'Oficina Pequeña',
-      imagen: imgprimerpiso,
+      titulo: 'Sillones',
+      imagen: img2,
       caracteristicas: [
-        'Ideal para 1 o 2 personas',
+        'Individual',
         'WiFi de alta velocidad',
-        'Climatización',
+        'Puerto USB',
         'Acceso 24hs',
       ],
     },
     {
-      titulo: 'Oficina Mediana',
-      imagen: imgsalareuniones,
+      titulo: 'Oficina',
+      imagen: img3,
       caracteristicas: [
-        'Para equipos de hasta 4 personas',
+        'Individual, se comparte con otras 3 personas',
         'TV Smart y Pizarra',
         'Vista al frente',
         'Ambiente silencioso',
       ],
     },
     {
-      titulo: 'Oficina Grande',
-      imagen: imgterraza,
+      titulo: 'Sala de Reuniones',
+      imagen: img4,
       caracteristicas: [
         'Capacidad hasta 8 personas',
         'Salón privado',
         'Proyector y pantalla',
         'Escritorios ergonómicos',
+      ],
+    },
+    {
+      titulo: 'Terraza',
+      imagen: img5,
+      caracteristicas: [
+        'Capacidad hasta 20 personas',
+        'Salón privado',
+        'Proyector y pantalla',
+        'Escritorios ergonómicos',
+      ],
+    },
+    {
+      titulo: 'Planta Baja Completa',
+      imagen: img6,
+      caracteristicas: [
+        'Capacidad hasta 20 personas',
+        'Salón privado',
+        'Proyector y pantalla',
+        'Ideal para Clases',
       ],
     },
   ];
